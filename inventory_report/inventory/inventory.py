297dd70194edf0fd8980a22d8ba26780b28f5e5e
Alex Silva <alexcssilva@gmail.com>
2022-09-21 22:39:36 -0300

    feat: update inventory.py - Desafio 07 concluido

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -2,10 +2,10 @@ from csv import DictReader
 from json import load
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
+import xml.etree.ElementTree as ET
 
 
-class Inventory():
-
+class Inventory:
     @classmethod
     def file_csv(cls, path):
         read_csv = DictReader(path, delimiter=",", quotechar='"')
@@ -17,11 +17,25 @@ class Inventory():
         return load(path)
 
     @classmethod
+    def file_xml(cls, path):
+        xml = ET.parse(path).getroot()
+        result = [
+            {element.tag: element.text for element in document}
+            for document in xml
+        ]
+        return result
+
+    # Como ler arquivo xml em python
+    # https://acervolima.com/ler-e-escrever-arquivos-xml-em-python/#:~:text=Lendo%20arquivos%20XML&text=Ent%C3%A3o%20peguei%20a%20raiz%20(tag,attrib%20.
+
+    @classmethod
     def file_type(clas, file, path):
         if path.endswith(".csv"):
             return Inventory.file_csv(file)
         if path.endswith(".json"):
             return Inventory.file_json(file)
+        if path.endswith(".xml"):
+            return Inventory.file_xml(file)
 
     @classmethod
     def import_data(cls, path, report):
